4f0baba07d9cbd0d60851e7c5bc1b348646d30c2
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-22 12:12:07 -0400

    added a constructor to the gamepanel class.

--- src/com/jeffwhenderson/tronRacer/view/GamePanel.java
+++ src/com/jeffwhenderson/tronRacer/view/GamePanel.java
@@ -1,10 +1,14 @@
 package com.jeffwhenderson.tronRacer.view;
 
+import java.awt.Dimension;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
+	private static final int DEFAULT_WIDTH = 800;
+	private static final int DEFAULT_HEIGHT = 600;
+	
 	public GamePanel() {
 		super();
-		
+		this.setSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
 	}
 }
