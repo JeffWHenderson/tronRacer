ec83f6b4b4af7631361530c28d2f481341f8b43d
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-22 11:53:48 -0400

    update the gambeboard class.

--- src/com/jeffwhenderson/tronRacer/view/GameBoard.java
+++ src/com/jeffwhenderson/tronRacer/view/GameBoard.java
@@ -1,5 +1,16 @@
 package com.jeffwhenderson.tronRacer.view;
 
-public class GameBoard {
+import javax.swing.JFrame;
 
+@SuppressWarnings("serial")
+public class GameBoard extends JFrame {
+	public GameBoard() {
+		super("JRacer");
+		start();
+	}
+	
+	private void start() {
+		 setVisible(true);
+		 
+	}
 }
