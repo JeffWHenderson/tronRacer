aa6b846b5311c44823f02b67d5715c07cb0e9b88
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-22 12:35:55 -0400

    added GUI stuff.

--- src/com/jeffwhenderson/tronRacer/view/GameBoard.java
+++ src/com/jeffwhenderson/tronRacer/view/GameBoard.java
@@ -1,20 +1,27 @@
 package com.jeffwhenderson.tronRacer.view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class GameBoard extends JFrame {
+	private GamePanel myGamePanel;
+	
 	public GameBoard() {
 		super("JRacer");
+		myGamePanel = new GamePanel();
 		start();
 	}
 	
 	public void start() {
 		 setVisible(true);	
-		 addGamePanel();
+		 buildGUI();
 	}
 	
-	private void addGamePanel() {
+	private void buildGUI() {
+		JPanel mainContainer = new JPanel();
+		mainContainer.add(myGamePanel);
 		
+		add(mainContainer);
 	}
 }
